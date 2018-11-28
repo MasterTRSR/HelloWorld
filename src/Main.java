d2e27ec830d1cf681cdb07001a9c352939311a0d
Sravan Reddy <sravan.reddy@lt5836.lithium.local>
2018-11-28 14:26:57 +0530

    Git practice - commands

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // arraylist
+        //  1 - 3  10 , 4  1
+        //  2 - 5  5 , 6  30
+        //  3 - 2  5 , 4  20
+        //  4 - 6  25 ,
+        Scanner s = new Scanner(System.in);
+        int N = s.nextInt();
+        int T = s.nextInt();
+
+
+        ArrayList<Integer[]>[] inputs = new ArrayList[N+1];
+
+        for(int i = 1;i<=T;i++){
+            int index = s.nextInt();
+            int node = s.nextInt();
+            int weight = s.nextInt();
+            Integer[] input = {node, weight};
+            if(inputs[index] == null)
+                inputs[index] = new ArrayList<>();
+            inputs[index].add(input);
+        }
+
+//        Queue<Integer> queue = new PriorityQueue<>();
+
+
+
+
+
+
+
+
+
+
+    }
+}
